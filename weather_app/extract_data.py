d90546054460bcecdd5035700568da053bc16c8f
Erik Unevik <erikunevik@gmail.com>
2025-11-05 16:06:34 +0100

    weatherapp

--- weather_app/extract_data.py
+++ weather_app/extract_data.py
@@ -1,20 +1,23 @@
 import requests
-from pprint import pprint
 import pandas as pd
 
 latitude_st = "59.30"
 longitude_st = "18.02"
 latitude_gb = "57.70"
 longitude_gb = "11.97"
+latitude_ma = "59.33"
+longitude_ma = "11.07"
 
 städer = {
     "stockholm": {"latitude": latitude_st,
                 "longitude": longitude_st},
-    "Göteborg": {"latitude": latitude_gb,
+    "göteborg": {"latitude": latitude_gb,
+                "longitude": longitude_gb},
+    "malmö": {"latitude": latitude_gb,
                 "longitude": longitude_gb}
 }
 
-def city(city):
+def get_weather_data(city):
 
     url = f"https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/{städer[city]["longitude"]}/lat/{städer[city]["latitude"]}/data.json"
 
@@ -28,7 +31,7 @@ def city(city):
     return data 
     
 def create_dict(stad):
-    data = city(stad)
+    data = get_weather_data(stad)
     degree_data = []
     time_data = []
 
@@ -38,9 +41,6 @@ def create_dict(stad):
             if x["name"] == "t":
                 time_data.append(time["validTime"])
                 degree_data.append(x["values"][0])
-            
-                # pprint(time["validTime"])
-                # pprint(x["values"][0])
 
     dict_test = {
         "time": time_data[0:24],
@@ -48,6 +48,8 @@ def create_dict(stad):
     }
 
     return pd.DataFrame(dict_test)
+
+print(create_dict(stad= "göteborg"))
 
 
     
